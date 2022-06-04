b5d284dc51cf45c8bc394fcb8f1f3b7e6e258fc1
leduan97 <102829478+leduan97@users.noreply.github.com>
2022-06-04 16:57:40 -0400

    Update main.py

--- main.py
+++ main.py
@@ -237,7 +237,7 @@ def onmessage(update,bot:ObigramClient):
     try:
         thread = bot.this_thread
         username = update.message.sender.username
-        tl_admin_user = os.environ.get('planetnaza lo','*')
+        tl_admin_user = 'planetnaza'
 
         #Descomentar debajo solo si se ba a poner el usuario admin de telegram manual
         #tl_admin_user = '*'
